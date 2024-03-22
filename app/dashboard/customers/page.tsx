86832d9a7f425a168566530e0b925f0fd507e177
Surieya <surieyasurieya@gmail.com>
2024-03-22 18:54:00 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Customers = (props: Props) => {
+  return (
+      <div>
+         Customers Page 
+    </div>
+  )
+}
+
+export default Customers
